a224d226358901667e93295b4d362a29f66d427f
Frusadev <frusadev@gmail.com>
2024-08-24 13:55:23 +0000

    Settings documentation

--- lua/settings.lua
+++ lua/settings.lua
@@ -6,28 +6,36 @@ local api = vim.api
 local cmd = vim.cmd
 local g = vim.g
 
+-- Indentation size
 opt.shiftwidth = 2
 opt.tabstop = 2
 opt.expandtab = true
+
+-- Termguicolors for better color accuracy
 opt.termguicolors = true
+
+-- Persistent undo (Undo even after closing the buffer)
 opt.undofile = true
 opt.undodir = os.getenv("HOME") .. "/.local/state/undo"
 
+-- Enable to number row by default
 o.number = true
 o.numberwidth = 2
-o.ruler = false
+o.ruler = true
 
 -- UI
--- Window borders
+  -- Window borders
 opt.fillchars:append({ vert = "▏" })
 opt.fillchars:append({ horiz = "⎯" })
--- These '~' on empty lines
+  -- These '~' on empty lines
 opt.fillchars:append({ eob = " " })
---
--- For pasting from system clipboard
+
+  -- For pasting from system clipboard
 opt.clipboard:append("unnamedplus")
 
+
 -- Running autocmds (applying frusavim settings)
+-- Colorscheme
 api.nvim_create_autocmd("VimEnter", {
 	callback = function()
 		cmd("colorscheme catppuccin")
