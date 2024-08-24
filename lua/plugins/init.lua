35ec5da01114c42ef7b27cae6fbb51f678463146
Frusadev <frusadev@gmail.com>
2024-08-24 15:37:58 +0000

    Add MasonInstallAll command

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -16,25 +16,11 @@ local plugins = {
 			"neovim/nvim-lspconfig",
 		},
 		automatic_installation = true,
-		ensure_installed = {
-			"pyright",
-			"debugpy",
-			"nimlsp",
-			"tsserver",
-			"nimlangserver",
-			"eslint-lsp",
-			"flake8",
-			"htmlhint",
-			"html-lsp",
-			"biome",
-			"jsonlint",
-			"jq",
-			"gopls",
-			"lua-language-server",
-			"v-analyzer",
-			"tailwindcss-language-server",
-			"stylua",
-		},
+		config = function()
+			require("mason").setup()
+			require("mason-lspconfig").setup({})
+		end,
+		lazy = false,
 	},
 	{
 		"hrsh7th/nvim-cmp",
@@ -179,8 +165,46 @@ local plugins = {
 		-- -- require("configs.mason")
 		--    end,
 		opts = {
-			ensure_installed = {},
+			ensure_installed = {
+				"pyright",
+				"debugpy",
+				"nimlsp",
+				"nimlangserver",
+				"eslint-lsp",
+				"flake8",
+				"htmlhint",
+				"html-lsp",
+				"biome",
+				"jsonlint",
+				"jq",
+				"gopls",
+				"lua-language-server",
+				"v-analyzer",
+				"tailwindcss-language-server",
+				"stylua",
+				"typescript-language-server",
+			},
 		},
+		config = function(_, opts)
+			require("mason").setup(opts)
+			vim.api.nvim_create_user_command("MasonInstallAll", function()
+				if opts.ensure_installed and #opts.ensure_installed > 0 then
+					vim.cmd("Mason")
+					local mr = require("mason-registry")
+
+					mr.refresh(function()
+						for _, tool in ipairs(opts.ensure_installed) do
+							local p = mr.get_package(tool)
+							if not p:is_installed() then
+								p:install()
+							end
+						end
+					end)
+				end
+			end, {})
+
+			vim.g.mason_binaries_list = opts.ensure_installed
+		end,
 	},
 	{
 		"zbirenbaum/nvterm",
