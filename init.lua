85a4c7cd47450fbd04f53ee2e904ee4b857497bd
Frusadev <frusadev@gmail.com>
2024-09-27 12:37:28 +0000

    setup nvim vscode plugin

--- init.lua
+++ init.lua
@@ -1,5 +1,10 @@
-require("config.lazy")
-require("mappings")
-require("settings")
-require("autocmds")
+if vim.g.vscode then
+  require("mappings")
+  require("config.lazycode")
+else
+  require("config.lazy")
+  require("mappings")
+  require("settings")
+  require("autocmds")
+end
 vim.opt.termguicolors = true
