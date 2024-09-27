46846767db45306521c0e28ec940abd6ab79a676
Frusadev <frusadev@gmail.com>
2024-09-27 12:42:37 +0000

    add venv select mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,13 +4,13 @@ vim.g.mapleader = " "
 vim.g.localleader = " "
 
 if vim.g.neovide then
-	-- Mapping for Ctrl+Shift+V to paste
-	map("n", "<C-S-v>", '"+p', { noremap = true, silent = true })
-	map("v", "<C-S-v>", '"+p', { noremap = true, silent = true })
-	map("i", "<C-S-v>", "<C-R>+", { noremap = true, silent = true })
+  -- Mapping for Ctrl+Shift+V to paste
+  map("n", "<C-S-v>", '"+p', { noremap = true, silent = true })
+  map("v", "<C-S-v>", '"+p', { noremap = true, silent = true })
+  map("i", "<C-S-v>", "<C-R>+", { noremap = true, silent = true })
 
-	-- Mapping for Ctrl+Shift+C to copy
-	map("v", "<C-S-c>", '"+y', { noremap = true, silent = true })
+  -- Mapping for Ctrl+Shift+C to copy
+  map("v", "<C-S-c>", '"+y', { noremap = true, silent = true })
 end
 
 map("i", "<C-b>", "<ESC>^i", { desc = "move beginning of line" })
@@ -37,13 +37,11 @@ map("n", "<leader>n", "<cmd>set nu!<CR>", { desc = "toggle line number" })
 map("n", "<leader>rn", "<cmd>set rnu!<CR>", { desc = "toggle relative number" })
 
 map("n", "<leader>fm", function()
-	require("conform").format({ lsp_fallback = true })
+  require("conform").format({ lsp_fallback = true })
 end, { desc = "format files" })
 
 -- python venv switch
-map("n", "<leader>cv", function()
-	require("swenv.api").pick_venv()
-end, { desc = "Change python venv" })
+map("n", "<leader>cv", "<cmd>VenvSelect<cr>", { desc = "Change python venv" })
 -- global lsp mappings
 map("n", "<leader>ds", vim.diagnostic.setloclist, { desc = "lsp diagnostic loclist" })
 
@@ -92,10 +90,10 @@ map("n", "<leader>pt", "<cmd>Telescope terms<CR>", { desc = "telescope pick hidd
 map("n", "<leader>th", "<cmd>Telescope colorscheme<CR>", { desc = "telescope themes/colorschemes" })
 map("n", "<leader>ff", "<cmd>Telescope find_files hidden=true<cr>", { desc = "telescope find files" })
 map(
-	"n",
-	"<leader>fa",
-	"<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
-	{ desc = "telescope find all files" }
+  "n",
+  "<leader>fa",
+  "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
+  { desc = "telescope find all files" }
 )
 
 map("n", "<leader>ps", "<cmd>PomodoroStart<CR>", { desc = "Start the pomodoro timer" })
@@ -107,13 +105,13 @@ local terminal = require("nvterm.terminal")
 local toggle_modes = { "n", "t" }
 
 map(toggle_modes, "<A-h>", function()
-	terminal.toggle("horizontal")
+  terminal.toggle("horizontal")
 end)
 map(toggle_modes, "<A-v>", function()
-	terminal.toggle("vertical")
+  terminal.toggle("vertical")
 end)
 map(toggle_modes, "<A-i>", function()
-	terminal.toggle("float")
+  terminal.toggle("float")
 end)
 map("t", "<C-x>", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
 
@@ -121,23 +119,23 @@ map("t", "<C-x>", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
 map("n", "<leader>wK", "<cmd>WhichKey <CR>", { desc = "whichkey all keymaps" })
 
 map("n", "<leader>wk", function()
-	vim.cmd("WhichKey " .. vim.fn.input("WhichKey: "))
+  vim.cmd("WhichKey " .. vim.fn.input("WhichKey: "))
 end, { desc = "whichkey query lookup" })
 
 -- blankline
 map("n", "<leader>cc", function()
-	local config = { scope = {} }
-	config.scope.exclude = { language = {}, node_type = {} }
-	config.scope.include = { node_type = {} }
-	local node = require("ibl.scope").get(vim.api.nvim_get_current_buf(), config)
+  local config = { scope = {} }
+  config.scope.exclude = { language = {}, node_type = {} }
+  config.scope.include = { node_type = {} }
+  local node = require("ibl.scope").get(vim.api.nvim_get_current_buf(), config)
 
-	if node then
-		local start_row, _, end_row, _ = node:range()
-		if start_row ~= end_row then
-			vim.api.nvim_win_set_cursor(vim.api.nvim_get_current_win(), { start_row + 1, 0 })
-			vim.api.nvim_feedkeys("_", "n", true)
-		end
-	end
+  if node then
+    local start_row, _, end_row, _ = node:range()
+    if start_row ~= end_row then
+      vim.api.nvim_win_set_cursor(vim.api.nvim_get_current_win(), { start_row + 1, 0 })
+      vim.api.nvim_feedkeys("_", "n", true)
+    end
+  end
 end, { desc = "blankline jump to current context" })
 map("n", "<leader>cw", ":close<CR>", { desc = "Close window" })
 
@@ -157,9 +155,9 @@ map("n", "<A-1>", ":resize +5<CR>", { desc = "Increase horizontal size by 5" })
 map("n", "<A-2>", ":resize -5<CR>", { desc = "Decrease horizontal size by 5" })
 
 map("n", "<leader>fm", function()
-	local conform = require("conform")
-	conform.format({ bufnr = vim.api.nvim_get_current_buf(), async = true })
-	conform.format({ bufnr = vim.api.nvim_get_current_buf(), async = true, lsp_fallback = true })
+  local conform = require("conform")
+  conform.format({ bufnr = vim.api.nvim_get_current_buf(), async = true })
+  conform.format({ bufnr = vim.api.nvim_get_current_buf(), async = true, lsp_fallback = true })
 end, { desc = "LSP and Conform formatting" })
 
 -- cdProject
@@ -169,66 +167,66 @@ map("n", "<leader>pd", "<cmd>CdProjectDelete<CR>", { desc = "Delete this dir as 
 -- LSP mappings
 
 map("n", "gD", function()
-	vim.lsp.buf.declaration()
+  vim.lsp.buf.declaration()
 end, { desc = "LSP declaration" })
 
 map("n", "gd", function()
-	vim.lsp.buf.definition()
+  vim.lsp.buf.definition()
 end, { desc = "LSP definition" })
 
 map("n", "K", function()
-	vim.lsp.buf.hover()
+  vim.lsp.buf.hover()
 end, { desc = "LSP hover" })
 
 map("n", "gi", function()
-	vim.lsp.buf.implementation()
+  vim.lsp.buf.implementation()
 end, { desc = "LSP implementation" })
 
 map("n", "<leader>ls", function()
-	vim.lsp.buf.signature_help()
+  vim.lsp.buf.signature_help()
 end, { desc = "LSP signature help" })
 
 map("n", "<leader>D", function()
-	vim.lsp.buf.type_definition()
+  vim.lsp.buf.type_definition()
 end, { desc = "LSP type definition" })
 
 vim.keymap.set("n", "<leader>ra", function()
-	return ":IncRename " .. vim.fn.expand("<cword>")
+  return ":IncRename " .. vim.fn.expand("<cword>")
 end, { expr = true })
 -- map("n", "<leader>ra", "<cmd>IncRename<CR>", { desc = "LSP rename" })
 
 map("n", "<leader>ca", function()
-	vim.lsp.buf.code_action()
+  vim.lsp.buf.code_action()
 end, { desc = "LSP code action" })
 
 map("n", "gr", function()
-	vim.lsp.buf.references()
+  vim.lsp.buf.references()
 end, { desc = "LSP references" })
 
 map("n", "<leader>lf", function()
-	vim.diagnostic.open_float({ border = "rounded" })
+  vim.diagnostic.open_float({ border = "rounded" })
 end, { desc = "Floating diagnostic" })
 
 map("n", "[d", function()
-	vim.diagnostic.goto_prev({ float = { border = "rounded" } })
+  vim.diagnostic.goto_prev({ float = { border = "rounded" } })
 end, { desc = "Goto previous diagnostic" })
 
 map("n", "]d", function()
-	vim.diagnostic.goto_next({ float = { border = "rounded" } })
+  vim.diagnostic.goto_next({ float = { border = "rounded" } })
 end, { desc = "Goto next diagnostic" })
 
 map("n", "<leader>q", function()
-	vim.diagnostic.setloclist()
+  vim.diagnostic.setloclist()
 end, { desc = "Diagnostic setloclist" })
 
 map("n", "<leader>wa", function()
-	vim.lsp.buf.add_workspace_folder()
+  vim.lsp.buf.add_workspace_folder()
 end, { desc = "Add workspace folder" })
 
 map("n", "<leader>wr", function()
-	vim.lsp.buf.remove_workspace_folder()
+  vim.lsp.buf.remove_workspace_folder()
 end, { desc = "Remove workspace folder" })
 
 map("n", "<leader>wl", function()
-	print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 end, { desc = "List workspace folders" })
