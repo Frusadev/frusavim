7a879c8a5d1611200bb1e99670c21a2632460ec8
Frusadev <frusadev@gmail.com>
2024-11-06 23:27:08 +0000

    Update nim and python lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,23 +1,25 @@
 local lspconfig = require("lspconfig")
-local servers = { "html", "basedpyright", "lua_ls", "gopls", "cssls", "ts_ls", "nimls", "v_analyzer", "tailwindcss" }
+local servers = {
+  "html",
+  "basedpyright",
+  "lua_ls",
+  "gopls",
+  "cssls",
+  "ts_ls",
+  "nimls",
+  "v_analyzer",
+  "tailwindcss",
+}
 
 lspconfig["basedpyright"].setup({
   settings = {
     python = {
       venvPath = "~/Workspace/sys/python/env/",
-      pythonPath = "~/Workspace/sys/python/env/bin/python"
-    }
-  }
+      pythonPath = "~/Workspace/sys/python/env/bin/python",
+    },
+  },
 })
 -- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup({})
 end
-
--- vim.api.nvim_create_autocmd("LspAttach", {
---   group = vim.api.nvim_create_augroup("UserLspConfig", {}),
---   callback = function (ev)
---     local opts = {buffer = ev.buf}
---     vim.keymap.set('<leader>', 'gd', vim.lsp.buf.definition(), opts)
---   end
--- })
