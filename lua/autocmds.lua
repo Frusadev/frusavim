abdc613f0e33f658acda83d78eca953c1a7ba579
Frusadev <frusadev@gmail.com>
2025-03-15 10:50:15 +0000

    Remove absent autocmds

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,8 +1,8 @@
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
-    if vim.fn.argc() == 0 then
-      vim.cmd("CdProject")
-    end
+    -- if vim.fn.argc() == 0 then
+    --   vim.cmd("CdProject")
+    -- end
   end,
 })
 
