90910870a2acd576fae529d7b1440dffcdf02506
Frusadev <frusadev@gmail.com>
2025-10-18 19:11:59 +0000

    style(lualine): change theme to tokyodark

--- lua/configs/lualine.lua
+++ lua/configs/lualine.lua
@@ -39,7 +39,7 @@ local config = {
 		-- Disable sections and component separators
 		component_separators = "",
 		section_separators = "",
-		theme = "tokyonight",
+		theme = "tokyodark",
 	},
 	sections = {
 		-- these are to remove the defaults
