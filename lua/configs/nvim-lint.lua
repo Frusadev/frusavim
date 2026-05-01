73555941d008fd411a6098f0702278d9df2f2dd2
Frusadev <frusadev@gmail.com>
2026-05-01 14:53:37 +0000

    feat: add phpstan & pint

--- lua/configs/nvim-lint.lua
+++ lua/configs/nvim-lint.lua
@@ -1,9 +1,10 @@
-require('lint').linters_by_ft = {
-  javascript = {"biomejs"},
-  typescript = {"biomejs"},
-  html = {"htmlhint"},
-  json = {"jsonlint"},
-  python = {"flake8"},
+require("lint").linters_by_ft = {
+  javascript = { "biomejs" },
+  typescript = { "biomejs" },
+  html = { "htmlhint" },
+  json = { "jsonlint" },
+  python = { "flake8" },
+  php = { "phpstan" },
 }
 
 vim.api.nvim_create_autocmd({ "BufRead", "TextChanged", "InsertLeave" }, {
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -25,6 +25,8 @@ return {
         "rust-analyzer",
         "nimlangserver",
         "intelephense",
+        "phpstan",
+        "pint",
       },
     },
     config = function(_, opts)
